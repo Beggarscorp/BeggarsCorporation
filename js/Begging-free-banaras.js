411b5f217b17950d99c5a3279d314d95c8e986e5
Beggarscorp <admin@beggarscorporation.com>
2024-06-16 14:59:33 +0530

    add some images make news,signature section

--- js/Begging-free-banaras.js
+++ js/Begging-free-banaras.js
@@ -1,1 +1,11 @@
-console.log("Begging-free-banaras");
+document.addEventListener('DOMContentLoaded', function() {
+    let slides = document.querySelectorAll('.sig-slide');
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide, 3000); 
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+});
